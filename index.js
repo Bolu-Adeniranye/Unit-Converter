7c5b5dfd416abeb0dd35fa4d0d9b8c50548533c6
bolu-sama <68526923+bolu-sama@users.noreply.github.com>
2024-06-14 21:41:10 +0100

    Complete

--- index.js
+++ index.js
@@ -4,21 +4,36 @@
 1 kilogram = 2.204 pound
 */
 
-const convertBtn = document.getElementById("convert-btn")
-const inputEl = document.getElementById("quantity")
-let lengthOutput = document.getElementById("length-output")
-let volumeOutput = document.getElementById("volume-output")
-let massOutput = document.getElementById("mass-output")
+const convertBtn = document.getElementById("convert-btn");
+const inputEl = document.getElementById("quantity");
+const lengthOutput = document.getElementById("length-output");
+const volumeOutput = document.getElementById("volume-output");
+const massOutput = document.getElementById("mass-output");
 
-
-convertBtn.addEventListener("click",function(){
-    renderLength(inputEl)
-    // renderVolume(inputEl)
-    // renderMass(inputEl)
-})
+convertBtn.addEventListener("click", function() {
+    const inputValue = inputEl.value;
+    renderLength(inputValue);
+    renderVolume(inputValue);
+    renderMass(inputValue);
+});
 
 function renderLength(convertNum) {
     convertNum = parseFloat(convertNum); // Convert input to float
-    let lengthOutput = (convertNum * 3.281).toFixed(3); // Multiply and then fix to 3 decimal places
-    return lengthOutput;
+    let lengthResult = (convertNum * 3.281).toFixed(3); // Multiply and then fix to 3 decimal places
+    let reverselengthResult = (convertNum / 3.281).toFixed(3); // Divide and then fix to 3 decimal places
+    lengthOutput.textContent = `${convertNum}m = ${lengthResult} feet | ${convertNum} feet = ${reverselengthResult}m`;
+}
+
+function renderVolume(convertNum) {
+    convertNum = parseFloat(convertNum); // Convert input to float
+    let volumeResult = (convertNum * 0.264).toFixed(3); // Multiply and then fix to 3 decimal places
+    let reversevolumeResult = (convertNum / 0.264).toFixed(3); // Divide and then fix to 3 decimal places
+    volumeOutput.textContent = `${convertNum}L = ${volumeResult} gallons | ${convertNum} gallons = ${reversevolumeResult}L`;
+}
+
+function renderMass(convertNum) {
+    convertNum = parseFloat(convertNum); // Convert input to float
+    let massResult = (convertNum * 2.204).toFixed(3); // Multiply and then fix to 3 decimal places
+    let reversemassResult = (convertNum / 2.204).toFixed(3); // Divide and then fix to 3 decimal places
+    massOutput.textContent = `${convertNum} Kg = ${massResult} lb | ${convertNum} lb = ${reversemassResult} Kg`;
 }
